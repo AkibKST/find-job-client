bed677d53dd4839a3461e75711cf6d48d68bb6cf
AkibKST <akibkst22@gmail.com>
2025-04-08 11:05:08 +0600

    Auth Interceptor using axios for unauthorized user and logged out the user in use Axios secure hook

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000/api",
@@ -6,6 +9,28 @@ const axiosInstance = axios.create({
 });
 
 const useAxiosSecure = () => {
+  const { signOutUser } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("api response error status", error.status);
+        if (error.status === 401 || error.status === 403) {
+          signOutUser()
+            .then(() => {
+              // redirect to the login page
+              navigate("/signIn");
+            })
+            .catch((err) => console.log(err));
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, []);
   return axiosInstance;
 };
 
